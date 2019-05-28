481042d74cb624c4f1eb0f311caa3d9d109e2024
Tran Phong Phu <phutp@sendo.vn>
2019-05-28 18:23:23 +0700

    Init project

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,11 +1,58 @@
 package cmd
 
 import (
-	"github.com/urfave/cli"
+  "os"
+  "os/signal"
+  "log"
+  "time"
+  "context"
+
+  "../model"
+
+	"go.uber.org/fx"
+  "github.com/urfave/cli"
+  "github.com/jinzhu/gorm"
 )
+
+func handleMigrateDB(db *gorm.DB) {
+  db.AutoMigrate(&model.Note{})
+}
+
+func migrateAction(appContext *cli.Context) {
+	app := fx.New(		
+	  fx.Provide(
+      func() *cli.Context{
+        return appContext
+      },
+      newLogger,
+      newDB,
+	  ),
+	  fx.Invoke(handleMigrateDB),
+	)
+  
+	startCtx := context.Background()
+	if err := app.Start(startCtx); err != nil {
+	  log.Fatal(err)
+	}
+  
+	// @Todo: This is not working
+	quit := make(chan os.Signal)
+	  signal.Notify(quit, os.Interrupt)
+	<-quit
+	
+	stopCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := app.Stop(stopCtx); err != nil {
+	  log.Fatal(err)
+	}
+}
 
 // Migrate is a definition of cli.Command used to migrate schema to database
 var Migrate cli.Command = cli.Command{
 	Name:  "migrate",
 	Usage: "Migrate db",
+	Action: func(appContext *cli.Context) error {
+    migrateAction(appContext)
+    return nil
+	},
 }
---
+++ god.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+gocode close
+gocode exit
+get -u github.com/mdempsky/gocode
