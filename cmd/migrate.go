90f02219f7124b2c8b807d0ba653141de2303e95
Tran Phong Phu <phutp@sendo.vn>
2019-05-28 18:28:05 +0700

    Init DI

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,50 +1,30 @@
 package cmd
 
 import (
-  "os"
-  "os/signal"
-  "log"
-  "time"
-  "context"
+	"../model"
 
-  "../model"
-
+	"github.com/jinzhu/gorm"
+	"github.com/urfave/cli"
 	"go.uber.org/fx"
-  "github.com/urfave/cli"
-  "github.com/jinzhu/gorm"
 )
 
 func handleMigrateDB(db *gorm.DB) {
-  db.AutoMigrate(&model.Note{})
+	db.AutoMigrate(&model.Note{})
 }
 
 func migrateAction(appContext *cli.Context) {
-	app := fx.New(		
-	  fx.Provide(
-      func() *cli.Context{
-        return appContext
-      },
-      newLogger,
-      newDB,
-	  ),
-	  fx.Invoke(handleMigrateDB),
+	app := fx.New(
+		fx.Provide(
+			func() *cli.Context {
+				return appContext
+			},
+			newLogger,
+			newDB,
+		),
+		fx.Invoke(handleMigrateDB),
 	)
-  
-	startCtx := context.Background()
-	if err := app.Start(startCtx); err != nil {
-	  log.Fatal(err)
-	}
-  
-	// @Todo: This is not working
-	quit := make(chan os.Signal)
-	  signal.Notify(quit, os.Interrupt)
-	<-quit
-	
-	stopCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-	if err := app.Stop(stopCtx); err != nil {
-	  log.Fatal(err)
-	}
+
+	app.Run()
 }
 
 // Migrate is a definition of cli.Command used to migrate schema to database
@@ -52,7 +32,7 @@ var Migrate cli.Command = cli.Command{
 	Name:  "migrate",
 	Usage: "Migrate db",
 	Action: func(appContext *cli.Context) error {
-    migrateAction(appContext)
-    return nil
+		migrateAction(appContext)
+		return nil
 	},
 }
--- cmd/start.go
+++ cmd/start.go
@@ -1,29 +1,27 @@
 package cmd
 
 import (
-	"github.com/jinzhu/gorm"
-  "context"
-  "log"
-  "os"
-  "time"
-  "os/signal"
+	"context"
+	"log"
 
-  "../handler"
-  "../server"
-  "go.uber.org/fx"
-  "github.com/urfave/cli"
+	"github.com/jinzhu/gorm"
+
+	"../handler"
+	"../server"
+	"github.com/urfave/cli"
+	"go.uber.org/fx"
 )
 
 // handleHTTPServer handle http server
 func handleHTTPServer(lc fx.Lifecycle, appContext *cli.Context, logger *log.Logger, db *gorm.DB) {
-  s := server.Server{
-    Engine:  handler.BuildEngine(),
-    Address: appContext.String("address"),
-    Port:    appContext.String("port"),
-  }
-  lc.Append(fx.Hook{
+	s := server.Server{
+		Engine:  handler.BuildEngine(),
+		Address: appContext.String("address"),
+		Port:    appContext.String("port"),
+	}
+	lc.Append(fx.Hook{
 		OnStart: func(context.Context) error {
-      err := s.Start()
+			err := s.Start()
 			return err
 		},
 		OnStop: func(ctx context.Context) error {
@@ -35,53 +33,39 @@ func handleHTTPServer(lc fx.Lifecycle, appContext *cli.Context, logger *log.Logg
 
 // startAction start command and init DI
 func startAction(appContext *cli.Context) {
-  app := fx.New(		
-    fx.Provide(
-      func() *cli.Context{
-        return appContext
-      },
-      newLogger,
-      newDB,
-    ),
-    fx.Invoke(handleHTTPServer),
-  )
+	app := fx.New(
+		fx.Provide(
+			func() *cli.Context {
+				return appContext
+			},
+			newLogger,
+			newDB,
+		),
+		fx.Invoke(handleHTTPServer),
+	)
 
-  startCtx := context.Background()
-  if err := app.Start(startCtx); err != nil {
-    log.Fatal(err)
-  }
-
-  // @Todo: This is not working
-  quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt)
-  <-quit
-  
-  stopCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-  defer cancel()
-  if err := app.Stop(stopCtx); err != nil {
-    log.Fatal(err)
-  }
+	app.Run()
 }
 
 // Start is a definition of cli.Command used to start gin server
 var Start = cli.Command{
-  Name: "start",
-  Flags: []cli.Flag{
-    cli.StringFlag{
-      Name:   "port, p",
-      Value:  "8080",
-      Usage:  "Listen on a port",
-      EnvVar: "PORT",
-    },
-    cli.StringFlag{
-      Name:   "address, a",
-      Value:  "0.0.0.0",
-      Usage:  "Bind to an address",
-      EnvVar: "ADDRESS",
-    },
-  },
-  Action: func(appContext *cli.Context) error {
-    startAction(appContext)
-    return nil
-  },
+	Name: "start",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:   "port, p",
+			Value:  "8080",
+			Usage:  "Listen on a port",
+			EnvVar: "PORT",
+		},
+		cli.StringFlag{
+			Name:   "address, a",
+			Value:  "0.0.0.0",
+			Usage:  "Bind to an address",
+			EnvVar: "ADDRESS",
+		},
+	},
+	Action: func(appContext *cli.Context) error {
+		startAction(appContext)
+		return nil
+	},
 }
