83c5fa4a91ce60e5bde6a4c776bdf86128f73cf7
Tran Phong Phu <vietean@gmail.com>
2019-06-06 00:00:44 +0700

    * Update helper testing

--- helper/pagination.go
+++ helper/pagination.go
@@ -1,23 +1,29 @@
 package helper
 
+// PaginationPageMax defines max value for page
+const PaginationPageMax = 100
+
+// PaginationLimitMax defines max value for limit
+const PaginationLimitMax = 100
+
 // Pagination struct
 type Pagination struct {
 	Page  uint `form:"p"`
 	Limit uint `form:"l"`
 }
 
-// GetPage to get current page
+// GetPage returns current page
 func (p Pagination) GetPage() uint {
 	if p.Page == 0 {
 		return 1
 	}
-	if p.Page > 100 {
-		return 100
+	if p.Page > PaginationPageMax {
+		return PaginationPageMax
 	}
 	return p.Page
 }
 
-// GetOffset to get current offset
+// GetOffset returns current offset
 func (p Pagination) GetOffset() uint {
 	page := p.GetPage()
 	limit := p.GetLimit()
@@ -25,10 +31,10 @@ func (p Pagination) GetOffset() uint {
 	return offset
 }
 
-// GetLimit to get limit
+// GetLimit returns limit of pagination
 func (p *Pagination) GetLimit() uint {
-	if p.Limit == 0 || p.Limit > 5 {
-		return 5
+	if p.Limit == 0 || p.Limit > PaginationLimitMax {
+		return PaginationLimitMax
 	}
 	return p.Limit
 }
---
+++ helper/pagination_test.go
@@ -0,0 +1,127 @@
+package helper
+
+import (
+	"testing"
+)
+
+func TestPagination_GetPage(t *testing.T) {
+	type fields struct {
+		Page  uint
+		Limit uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   uint
+	}{
+		{
+			"Get Page with valid data",
+			fields{10, 10},
+			10,
+		},
+		{
+			"Get Page with valid data",
+			fields{100, 10},
+			100,
+		},
+		{
+			"Get Page with invalid data",
+			fields{101, 10},
+			PaginationPageMax,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Pagination{
+				Page:  tt.fields.Page,
+				Limit: tt.fields.Limit,
+			}
+			if got := p.GetPage(); got != tt.want {
+				t.Errorf("Pagination.GetPage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPagination_GetLimit(t *testing.T) {
+	type fields struct {
+		Page  uint
+		Limit uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   uint
+	}{
+		{
+			"Get Limit with valid data",
+			fields{10, 10},
+			10,
+		},
+		{
+			"Get Limit with valid data",
+			fields{10, 100},
+			100,
+		},
+		{
+			"Get Limit with invalid data",
+			fields{10, 101},
+			PaginationLimitMax,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Pagination{
+				Page:  tt.fields.Page,
+				Limit: tt.fields.Limit,
+			}
+			if got := p.GetLimit(); got != tt.want {
+				t.Errorf("Pagination.GetLimit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPagination_GetOffset(t *testing.T) {
+	type fields struct {
+		Page  uint
+		Limit uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   uint
+	}{
+		{
+			"Get Offset with valid data",
+			fields{10, 10},
+			90,
+		},
+		{
+			"Get Offset with valid data",
+			fields{10, 100},
+			900,
+		},
+		{
+			"Get Offset with invalid data",
+			fields{10, 101},
+			(10 - 1) * PaginationLimitMax,
+		},
+		{
+			"Get Offset with invalid data",
+			fields{101, 10},
+			(PaginationLimitMax - 1) * 10,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Pagination{
+				Page:  tt.fields.Page,
+				Limit: tt.fields.Limit,
+			}
+			if got := p.GetOffset(); got != tt.want {
+				t.Errorf("Pagination.GetOffset() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
