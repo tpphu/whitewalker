240db406e68561f6d9d69ef3dc6c07437202e43a
Tran Phong Phu <phutp@sendo.vn>
2019-06-12 19:08:00 +0700

    * Update code con dang do

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,15 +1,37 @@
 package cmd
 
 import (
-	"github.com/tpphu/whitewalker/model"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
 
 	"github.com/urfave/cli"
 )
 
 func migrateAction(appContext *cli.Context) {
-	db := newDB(appContext)
-	defer db.Close()
-	db.AutoMigrate(&model.Note{}, &model.User{}, &model.Department{})
+	// db := newDB(appContext)
+	// defer db.Close()
+	subPackage := "model"
+	set := token.NewFileSet()
+	packs, err := parser.ParseDir(set, subPackage, nil, 0)
+	if err != nil {
+		fmt.Println("Failed to parse package:", err)
+		os.Exit(1)
+	}
+
+	funcs := []*ast.FuncDecl{}
+	for _, pack := range packs {
+		for _, f := range pack.Scopes() {
+			for _, d := range f.Decls {
+				if fn, isFn := d.(*ast.StructType); isFn {
+					funcs = append(funcs, fn)
+				}
+			}
+		}
+	}
+	//db.AutoMigrate(&model.Note{}, &model.User{}, &model.Department{})
 }
 
 // Migrate is a definition of cli.Command used to migrate schema to database
