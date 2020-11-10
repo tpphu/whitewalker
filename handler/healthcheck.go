ce5d40bf3b02d2f97f43da913b84cb55a80244de
Tran Phong Phu <vietean@gmail.com>
2020-11-10 21:31:56 +0700

    Update healthcheck.go

--- handler/healthcheck.go
+++ handler/healthcheck.go
@@ -25,6 +25,6 @@ func (h healthCheckHandlerImpl) ping(c iris.Context) {
 	c.JSON(iris.Map{
 		"sucess":   true,
 		"database": result.Ping == "1",
-		"message":  "pong 4",
+		"message":  "pong 5",
 	})
 }
